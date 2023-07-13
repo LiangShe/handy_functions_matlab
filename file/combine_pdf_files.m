fd28852342219d110dd7dfe6430f331c48abe547
LiangShe <liangsher@gmail.com>
2023-07-13 10:40:15 -0700

    Create combine_pdf_files.m

---
+++ file/combine_pdf_files.m
@@ -0,0 +1,26 @@
+function combine_pdf_files(output_file, filespec)
+% combine_pdf_files(output_file, filespec)
+
+if nargin<1
+    output_file = 'combined.pdf';
+end
+
+if nargin<2
+    filespec = '*.pdf';
+end
+
+if iscell(filespec)
+    files = filespec;
+elseif ischar(filespec)
+    files = find_files( filespec );
+end
+
+if exist(output_file,'file');
+    delete(output_file);
+end
+
+%% combine
+append_pdfs(output_file, files{:});
+
+
+end
